d6ca99b1b0f15d6796b1bfdaabb6b68b44000b24
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2015-08-06 19:08:33 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlutterMerchant::Application.config.secret_key_base = '3ca25a98fc8ffbd48f5fd608a166f0ca73bedad60289761ef2d3e506ed4a5d331bce894b2d6f77c7f1a4d357ca8062514bcaefb305af9b01a37bbcdd222e1111'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FlutterMerchant::Application.config.session_store :cookie_store, key: '_flutter-merchant_session'
