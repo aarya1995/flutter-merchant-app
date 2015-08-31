b67c6d8ca68becb40c540c0767e11962db2a912c
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2015-08-31 10:57:27 -0400

    fixing sendgrid issues

--- config/environment.rb
+++ config/environment.rb
@@ -6,8 +6,8 @@ FlutterMerchant::Application.initialize!
 
 # should store these credentials in environment variables on the server
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'aarya',
-  :password => '5kv6asfq',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
